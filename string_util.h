35c1753a55b5d32f9010e21d1f3e3e3bce049c93
sss <songsensen1@163.com>
2026-08-18 14:37:09 +0800

    add has str

--- string_util.h
+++ string_util.h
@@ -326,6 +326,10 @@ namespace crypto{
         return false;
     }
 
+    inline bool has_str(const char* originStr, std::string_view targetStr) {
+        return std::strstr(originStr, targetStr.data()) != nullptr;
+    }
+
     inline std::vector<std::string>& split(const std::string& s, char delim, std::vector<std::string>& elems) {
         std::stringstream ss(s);
         std::string item;
